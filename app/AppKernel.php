69a82751be86077091de11506d720d718e2fab6b
victor <bocharsky.bw@gmail.com>
2014-08-20 16:18:36 +0300

    Add LiipImagineBundle with filters

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
+            new Liip\ImagineBundle\LiipImagineBundle(),
+
             new BW\RouterBundle\BWRouterBundle(),
             new BW\DefaultBundle\BWDefaultBundle(),
             new BW\BlogBundle\BWBlogBundle(),
